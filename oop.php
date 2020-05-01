f0d3a3b22b9dbad07f55282723b905ebbeef086e
CharlZ <charles.ceo2020@gmail.com>
2020-05-01 04:22:37 -0600

    code cleanup 2

--- oop.php
+++ oop.php
@@ -6,25 +6,12 @@
   ini_set( 'display_errors', 1 );
   ini_set( 'display_startup_errors', 1 );
   error_reporting( E_ALL );
-  /**
-   * A basic object.
-   */
-  // $myObject = new stdClass(); // "new" keyword is REQUIRED when making an object from a CLASS (or BLUEPRINT.)
-  // // To add a property to our object, we use the -> arrow syntax.
-  // $myObject->name      = 'Jim Bob-Bob';
-  // $myObject->age       = 41;
-  // $myObject->interests = array( 'PHP', 'CSS' );
+ 
   /**
    * Include our class / blueprint file, so we can use our class.
    */
   include './includes/Blog.Class.php';
-  // Let's make a blog...
-  $cheetos       = new Blog( 'Cheetos', 3.99, 'Chip' );
-  $gushers       = new Blog( 'Fruit Gushers', 2.56, 'Fruit' );
-  $jollyRanchers = new Blog( 'Jolly Ranchers', 1.25, 'Fruit' );
-  $sharwarma     = new Blog( 'Sharwarma', 7.86, 'Wrap' );
-  // Let's throw them in an array for easy output...
-  $blogs = array( $cheetos, $gushers, $jollyRanchers, $sharwarma );
+
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
